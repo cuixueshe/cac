06dc38e559fe098032e0cad2d16969949b70ac02
v_jsliang <v_jsliang@tencent.com>
2022-07-05 11:44:05 +0800

    feat: index.spec中补下单测，在驱动出utils跟option的功能

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -18,7 +18,7 @@ test('basic-usage', () => {
   });
 });
 
-test('type value should be equal to node', () => {
+test.todo('type value should be equal to node', () => {
   const cli = cac();
 
   cli.option('--type <type>', 'Choose a project type', {
